335ee87b2df49773a729b5fbe7bd562582885279
Andrew Reitz <aj.reitz@gmail.com>
2014-11-19 18:12:31 -0600

    Added LazyDeveloperException

---
+++ src/main/java/lol/LazyDeveloperException.java
@@ -0,0 +1,7 @@
+package lol;
+
+/** Throw me whenever the fuck you want. */
+final public class LazyDeveloperException extends RuntimeException {
+    /** No arguments needed, we know who's fault it is. */
+    public LazyDeveloperException() {}
+}
