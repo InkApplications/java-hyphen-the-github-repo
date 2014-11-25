6ab928d1c0745d7502be13f2c7cf54791c4e6276
Andrew Reitz <aj.reitz@gmail.com>
2014-11-25 14:02:58 -0600

    Created disapoint annotation.

---
+++ src/main/java/lol/ಠ__ಠ.java
@@ -0,0 +1,21 @@
+package lol;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+/** That annotation for when you just don't have words. */
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, CONSTRUCTOR, TYPE, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
+public @interface ಠ__ಠ {
+
+}
