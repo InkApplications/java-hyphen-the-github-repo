07e93483b40245a479484d07b6714450a8bba35f
Andrew Reitz <aj.reitz@gmail.com>
2014-11-25 14:38:23 -0600

    Added retention, targets, and javadocs (in markdown)

--- src/main/java/lol/Pokemon.java
+++ src/main/java/lol/Pokemon.java
@@ -1,4 +1,26 @@
 package lol;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+/**
+ * Documentation annotation telling others, we just want to catch them all.
+ *
+ * Example usage:
+ * ```java
+ * try {
+ *   throw new Exception("YOLO");
+ * } catch (@Pokemon Exception e) {
+ *   System.out.println("We got an error " + e.getMessage);
+ * }
+ * ```
+ */
+@Documented
+@Retention(SOURCE)
+@Target({PARAMETER})
 public @interface Pokemon {
 }
