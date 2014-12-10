8b1cb81430608d46ed0c8b91116e8a59c5d76798
Andrew Reitz <aj.reitz@gmail.com>
2014-12-10 11:40:12 -0600

    Website at Wed Dec 10 11:40:12 CST 2014

--- README.md
+++
@@ -1,7 +0,0 @@
-# Ink Applications Java Template
-
-This is a template for getting started on Java Projects with gradle.
-
-## Usage
-
-*TODO*
--- src/main/java/lol/LazyDeveloperException.java
+++
@@ -1,7 +0,0 @@
-package lol;
-
-/** Throw me whenever the fuck you want. */
-final public class LazyDeveloperException extends RuntimeException {
-    /** No arguments needed, we know who's fault it is. */
-    public LazyDeveloperException() {}
-}
